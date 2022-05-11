a1c34f829d01311a55e5dc16fa91627fcc22a972
stikkireddy <sri.tikkireddy@databricks.com>
2022-05-11 07:38:20 -0400

    skip job submission if table doesnt need to be unloaded

--- table_acl_ext/jdbc/sql.py
+++ table_acl_ext/jdbc/sql.py
@@ -4,6 +4,7 @@ from sql_metadata import Parser
 
 from table_acl_ext.jdbc import TABLE_ID_WIDGET_PARAM, spark
 from table_acl_ext.jdbc.control import DatabaseTable, SynapseTable
+from table_acl_ext.jdbc.unload import should_i_unload
 from table_acl_ext.jobs import par_execute
 
 
@@ -16,7 +17,8 @@ def find_synapse_tables(sql_stmt):
     tables = get_tables(sql_stmt)
     synapse_tables = SynapseTable.list()
     return [SynapseTable.find(synapse_tables, table) for table in tables
-            if SynapseTable.find(synapse_tables, table) is not None]
+            if SynapseTable.find(synapse_tables, table) is not None and
+            should_i_unload(spark, SynapseTable.find(synapse_tables, table)) is True]
 
 
 def sql(sql_stmt):
