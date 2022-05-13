e0719e711fac0a6c82d0d915eb527ebf44846d14
stikkireddy <sri.tikkireddy@databricks.com>
2022-05-13 09:44:07 -0400

    support for storage account key

--- table_acl_ext/storage/adls.py
+++ table_acl_ext/storage/adls.py
@@ -42,3 +42,32 @@ class ADLSSPCredentialsManager(CredentialsManager):
             TextWidget(self.TENANT_ID, "", "Azure AAD Tenant ID"),
             TextWidget(self.GROUPS, "", "Groups"),
         ]
+
+
+class ADLSAccessKeyCredentialsManager(CredentialsManager):
+    ACCOUNT_NAME = "account_name"
+    ACCOUNT_KEY = "account_key"
+    GROUPS = "groups"
+
+    def get_databricks_secrets(self) -> List[DatabricksSecret]:
+        widgets = self.widgets()
+        widget_values = {widget.get_name(): widget.get() for widget in widgets}
+        groups = widget_values[self.GROUPS]
+        secret_value = SerializableStorageWrapper(AzureBlobFileSystem, options={
+            self.ACCOUNT_NAME: widget_values[self.ACCOUNT_NAME],
+            self.ACCOUNT_KEY: widget_values[self.ACCOUNT_KEY],
+        }).serialized_fs
+        return [
+            DatabricksSecret(
+                scope=get_scope(group),
+                key=widget_values[self.ACCOUNT_NAME],
+                secret_string=secret_value,
+                group=group
+            ) for group in groups.split(",")]
+
+    def widgets(self) -> List[Widget]:
+        return [
+            TextWidget(self.ACCOUNT_NAME, "", "Storage Account Name"),
+            TextWidget(self.ACCOUNT_KEY, "", "Storage Account Key"),
+            TextWidget(self.GROUPS, "", "Groups"),
+        ]
