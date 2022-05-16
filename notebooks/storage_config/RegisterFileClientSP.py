dbd8ae6e1dd7177f2e13052d4819f24d6dfdde7a
stikkireddy <sri.tikkireddy@databricks.com>
2022-05-16 11:40:08 -0400

    clean up and add example at bottom

--- notebooks/storage_config/RegisterFileClientSP.py
+++ notebooks/storage_config/RegisterFileClientSP.py
@@ -20,3 +20,14 @@ dbutils.notebook.exit()
 # COMMAND ----------
 
 manager.register()
+
+# COMMAND ----------
+
+dbutils.notebook.exit()
+
+# COMMAND ----------
+
+from table_acl_ext.storage import list_storages, get_client
+list_storages()
+client = get_client("<storage key>")
+client.ls("/")
